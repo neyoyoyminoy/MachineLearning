54f3679f551f3a04ea1da656c5fc1b643268c47f
neyoyoyminoy <167467364+neyoyoyminoy@users.noreply.github.com>
2025-09-16 10:46:56 -0600

    Update HW1problem6V1.py

--- HW1problem6V1.py
+++ HW1problem6V1.py
@@ -3,13 +3,16 @@
 #Homework 1
 #Create a noisy version Xn by corrupting each pixel independently with probability 5%, replacing it with 0 or 255. Perform SVD on Xn and construct low-rank approximations
 #Yk = QkQk'Xn
+#kset := {1, 51, 101, . . . , 784}
 
 import numpy as np
 
-n = 250
-while n >= 0:
+Xn = np.random.binomial(n = 1, p = 0.05, size = 748) #starting at 1, probability of 5%, and the max vector size of the MNIST is 748 (28*28)
+
+n = 0
+
+while n <= 784:
   n += 1
-    continue
   print(n)
 else:
   print('loop stopped')
