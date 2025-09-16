1ad259cd8a2c47d60f85e2faf3903f2decb06c7b
neyoyoyminoy <167467364+neyoyoyminoy@users.noreply.github.com>
2025-09-16 10:17:19 -0600

    Update HW1problem6V1.py

--- HW1problem6V1.py
+++ HW1problem6V1.py
@@ -2,10 +2,14 @@
 #Intro to Machine Learning  Fall 2025
 #Homework 1
 #Create a noisy version Xn by corrupting each pixel independently with probability 5%, replacing it with 0 or 255. Perform SVD on Xn and construct low-rank approximations
-Yk = QkQk'Xn
+#Yk = QkQk'Xn
 
 import numpy as np
-from tensorflow.keras.datasets import mnist
 
-n = 1
-while n >= 
+n = 250
+while n >= 0:
+  n += 1
+    continue
+  print(n)
+else:
+  print('loop stopped')
