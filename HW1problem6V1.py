ec579c72578c1627da339d007bc15a81996263ed
neyoyoyminoy <167467364+neyoyoyminoy@users.noreply.github.com>
2025-09-16 10:01:46 -0600

    Create HW1problem6V1.py

---
+++ HW1problem6V1.py
@@ -0,0 +1,11 @@
+#Brendan Nellis   ujf306
+#Intro to Machine Learning  Fall 2025
+#Homework 1
+#Create a noisy version Xn by corrupting each pixel independently with probability 5%, replacing it with 0 or 255. Perform SVD on Xn and construct low-rank approximations
+Yk = QkQk'Xn
+
+import numpy as np
+from tensorflow.keras.datasets import mnist
+
+n = 1
+while n >= 
