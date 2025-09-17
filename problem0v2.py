81f023c4c7ce46016ee0dbd17074d616b92d56c2
neyoyoyminoy <167467364+neyoyoyminoy@users.noreply.github.com>
2025-09-16 21:53:19 -0500

    Update problem0v2.py

--- problem0v2.py
+++ problem0v2.py
@@ -10,9 +10,20 @@ from tensorflow.keras.datasets import mnist #given from Dr. Markopoulos
 
 (trainX, trainY), _ = mnist.load_data() #this will load MNIST data into the variables, leaving the 'test' set blank for simplicity
 
-trainYmask = np.isin(trainY, [0,1,9]) #extracts only 0, 1, and 9 for analysis
+mask = np.isin(trainY, [0,1,9]) #extracts only 0, 1, and 9 for analysis
 
+trainingImages = trainX[mask] #grabbing the masked images for vectorization
+
+vTrainingImages = trainingImages.reshape(trainingImages.shape[0], -1) #vectorized images of 0, 1, and 9
+
+matrixX = vTrainingImages.astype('float32') #sets matrix X to float32
+
+
+print("Matrix X:", X.shape) #the final values of matrix X
+
+'''
 print("Xtrain: " + str(trainX.shape)) #prints the trained vector
 print("Ytrain: " + str(trainY.shape))
 
 print("trainYmask: " + str(trainYmask.shape)) #prints the shape of the mask, this should be the same as the og array
+'''
